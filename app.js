120d3e8b98762d248fb56e393f57e16dac0952ea
Dearie <eburujacintha@gmail.com>
2024-12-01 06:03:24 +0100

    fixed my connection bug

---
+++ app.js
@@ -0,0 +1,89 @@
+var express = require("express");
+let app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.set('json spaces', 3);
+const path = require('path');
+let PropertiesReader = require("properties-reader");
+// Load properties from the file
+let propertiesPath = path.resolve(__dirname, "db.properties");
+let properties = PropertiesReader(propertiesPath);
+
+// Extract values from the properties file
+let dbPprefix = properties.get("db.prefix");
+//URL-Encoding of User and PWD
+//for potential special characters
+let dbUsername = encodeURIComponent(properties.get("db.user"));
+let dbPwd = encodeURIComponent(properties.get("db.pwd"));
+let dbName = properties.get("db.dbName");
+let dbUrl = properties.get("db.dbUrl");
+let dbParams = properties.get("db.params");
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// MongoDB connection URL
+const uri = dbPprefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+
+let db1;//declare variable
+
+async function connectDB() {
+  try {
+    client.connect();
+    console.log('Connected to MongoDB');
+    db1 = client.db('FullStack');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+}
+
+connectDB(); //call the connectDB function to connect to MongoDB database
+
+//Optional if you want the get the collection name from the Fetch API in test3.html then
+app.param('collectionName', async function(req, res, next, collectionName) { 
+    req.collection = db1.collection(collectionName);
+    /*Check the collection name for debugging if error */
+    console.log('Middleware set collection:', req.collection.collectionName);
+    next();
+});
+
+// Ensure this route is defined after the middleware app.param
+// get all data from our collection in Mongodb
+app.get('/collections/:collectionName', async function(req, res, next) {
+    
+});
+
+app.get('/collections1/:collectionName', async function(req, res, next) {
+ 
+});
+
+app.get('/collections/:collectionName/:max/:sortAspect/:sortAscDesc', async function(req, res, next){
+    
+});
+
+app.get('/collections/:collectionName/:id' , async function(req, res, next) {
+    
+});
+
+app.post('/collections/:collectionName', async function(req, res, next) {
+    
+});
+
+app.delete('/collections/:collectionName/:id', async function(req, res, next) {
+    
+});
+
+app.put('/collections/:collectionName/:id', async function(req, res, next) {
+
+});
+
+app.use((err, req, res, next) => {
+    console.error('Global error handler:', err);
+    res.status(500).json({ error: 'An error occurred' });
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
